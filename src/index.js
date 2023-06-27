d8eadf19925fb61392ac40cc6ae6caae0296e678
Sergio Gomes <sergiopgomes92@gmail.com>
2023-06-27 10:16:23 -0300

    feat: :sparkles: loop through the locations to find their timezone

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 const { find } = require('geo-tz')
 
-console.log(find(47.650499, -122.350070))  // ['America/Los_Angeles']
-console.log(find(43.839319, 87.526148))  // ['Asia/Shanghai', 'Asia/Urumqi']
+const { locationList } = require('./locations')
+
+const success = []
+const errors = []
+
+locationList.forEach(location => {
+  try {
+    const result = find(location.geo[0], location.geo[1])
+    success.push({
+      id: location.id,
+      tz: result[0]
+    })
+  } catch (error) {
+    console.error(location.id, error)
+    errors.push({
+      id: location.id,
+      error: error.message
+    })
+  }
+})
+
+console.log('** Success **', success)
+console.log('** Error **', errors)
+
+// console.log(find(47.650499, -122.350070))  // ['America/Los_Angeles']
+// console.log(find(43.839319, 87.526148))  // ['Asia/Shanghai', 'Asia/Urumqi']
